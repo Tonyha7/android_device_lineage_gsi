314714bc8c3772fd55a682a1bf2878302a07827f
th7 <69128819+Tonyha7@users.noreply.github.com>
2023-03-24 13:11:12 +0800

    gsi: Set PRODUCT_CHARACTERISTICS to tablet

--- lineage_gsi_arm64_base.mk
+++ lineage_gsi_arm64_base.mk
@@ -6,6 +6,6 @@ $(call inherit-product, vendor/hardware_overlay/overlay.mk)
 PRODUCT_DEVICE := lineage_gsi_arm64
 PRODUCT_MODEL := LineageOS GSI on ARM64
 
-PRODUCT_CHARACTERISTICS := device
+PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS :=
